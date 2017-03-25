535878d6f806ebe9f006f2045fe365edd412b459
egmattison <egmattison@Emily-Mattisons-MacBook-Pro.local>
2017-03-24 20:48:56 -0400

    Added mockups

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $(function () {
+//   var source = $("#book-example").html();
+//   var template = Handlebars.compile(source);
+//
+//   var context = {
+//     books: [{
+//       title: 'Harry Potter',
+//       author: 'JK Rowling',
+//       cover: 'harrypotter',
+//       link: 'hp'
+//     },{
+//       title: 'The Newest Book',
+//       author: 'Ian Mcguire',
+//       cover: 'northwater'
+//     }]
+//   };
+//
+//   var compiled = template(context);
+//   $('.book-container').append(compiled);
+// });
